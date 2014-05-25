51726f5bdf4364034be749c045ca440560116b4b
Eric Newport <kethinov@gmail.com>
2014-05-24 21:25:17 -0700

    closes #1

--- app.js
+++ app.js
@@ -1,52 +1,51 @@
 // modules
 var express = require('express'), // express http server
-    adaro = require('adaro'),     // dust support for express
-    app = express();              // initialize express
+    morgan = require('morgan'), // express logger
+    bodyParser = require('body-parser'), // express body parser
+    methodOverride = require('method-override'), // express body parser
+    adaro = require('adaro'), // dust support for express
+    app = express(); // initialize express
 
-// configure express
-app.configure(function() {
+// set port
+app.set('port', process.env.NODE_PORT || 8000);
 
-  // set port
-  app.set('port', process.env.NODE_PORT || 80);
+// set views dir (dust templates)
+app.set('views', 'templates');
+app.engine('dust', adaro.dust());
+app.set('view engine', 'dust');
 
-  // set views dir (dust templates)
-  app.set('views', 'templates');
-  app.engine('dust', adaro.dust());
-  app.set('view engine', 'dust');
+// dumps http requests to the console
+app.use(morgan());
 
-  // dumps http requests to the console
-  app.use(express.logger());
+// defines req.body by parsing http requests (does NOT deal with multipart forms... see modules like node-formidable for that)
+app.use(bodyParser());
 
-  // defines req.body by parsing http requests (does NOT deal with multipart forms... see modules like node-formidable for that)
-  app.use(express.json());
-  app.use(express.urlencoded());
+// enables PUT and DELETE requests via <input type='hidden' name='_method' value='put'> and suchlike
+app.use(methodOverride());
 
-  // map statics
-  app.use(express.static(__dirname + '/statics/'));
-  app.use(app.router);
+// map statics
+app.use(express.static(__dirname + '/statics/'));
 
-  // homepage
-  app.all('/', function(req, res) {
-    res.render('index', {
-      content: {
-        title: 'Hello World!',
-        helloworld: 'Hello World!'
-      }
-    });
+// homepage
+app.route('/').all(function(req, res) {
+  res.render('index', {
+    content: {
+      title: 'Hello World!',
+      helloworld: 'Hello World!'
+    }
   });
+});
 
-  // 404 page
-  app.all('*', function(req, res) {
-    res.render('404', {
-      content: {
-        title: '404 Not Found',
-        heading: 'Not Found',
-        details1: 'The requested URL ',
-        details2: ' was not found on this server.'
-      }
-    });
+// 404 page
+app.route('*').all(function(req, res) {
+  res.render('404', {
+    content: {
+      title: '404 Not Found',
+      heading: 'Not Found',
+      details1: 'The requested URL ',
+      details2: ' was not found on this server.'
+    }
   });
-
 });
 
 app.listen(app.get('port'), function() {
